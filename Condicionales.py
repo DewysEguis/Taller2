21f5a4af389a7d8f94c4a3148b6f9e98d0a6276d
Dewys Eguis <deguis@mangus.org>
2021-09-13 15:48:52 -0500

    Punto n° 2

--- Condicionales.py
+++ Condicionales.py
@@ -12,3 +12,14 @@ def pto1(n_ca, t_co):
 
 des_1, tot_1 = pto1(4, 100000)
 print(f'Desucento: {des_1} ... Total a pagar por la compra de camisas: {tot_1}')
+
+#Punto 2
+
+def pto2 (num, t_com):
+
+    if num >= 74:
+        desc = t_com * 0.2
+    else:
+        desc = t_com * 0.15
+
+    return desc
